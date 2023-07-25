36e2467894f39e1b30ef3107bf2cb8dd57bb815e
Massinelo <massi.ait@gmail.com>
2023-07-25 17:30:42 +0200

    test

---
+++ itp3-storage-api/src/main/java/co/simplon/itp3/storageapi/controllers/Controller.java
@@ -0,0 +1,24 @@
+package co.simplon.itp3.storageapi.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/profiles")
+public class Controller {
+
+    @Value("${supportrequest-api.env}")
+    private String profile;
+
+    @Value("${supportrequest-api.commun}")
+    private String commun;
+
+    @GetMapping("/test")
+    public String test() {
+	String result = profile + commun;
+	return result;
+    }
+
+}
