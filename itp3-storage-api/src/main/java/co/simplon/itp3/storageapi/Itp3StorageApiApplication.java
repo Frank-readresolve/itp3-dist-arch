b2fe5bb568de3d04849a9a10309ea6614db4403a
Massinelo <massi.ait@gmail.com>
2023-07-26 10:44:40 +0200

    test

--- itp3-storage-api/src/main/java/co/simplon/itp3/storageapi/Itp3StorageApiApplication.java
+++ itp3-storage-api/src/main/java/co/simplon/itp3/storageapi/Itp3StorageApiApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Itp3StorageApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Itp3StorageApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication
+		.run(Itp3StorageApiApplication.class, args);
+	System.out.println("environement de " + args[0]);
+    }
 
 }
