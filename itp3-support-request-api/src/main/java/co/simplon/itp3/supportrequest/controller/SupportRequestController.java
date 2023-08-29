8d89444e621dd670056f2a6740543b8455051c6d
Massinelo <massi.ait@gmail.com>
2023-08-29 16:29:33 +0200

    feat: add cors policy to api gateway

--- itp3-support-request-api/src/main/java/co/simplon/itp3/supportrequest/controller/SupportRequestController.java
+++ itp3-support-request-api/src/main/java/co/simplon/itp3/supportrequest/controller/SupportRequestController.java
@@ -3,7 +3,6 @@ package co.simplon.itp3.supportrequest.controller;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +13,19 @@ import co.simplon.itp3.supportrequest.dtos.CreateSupportRequestDto;
 
 @RestController
 @RequestMapping("/support-request")
-@CrossOrigin
 public class SupportRequestController {
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void create (
-		@RequestBody @Valid CreateSupportRequestDto inputs) {
-		
-	        CreateSupportRequestDto supportRequestDto = new CreateSupportRequestDto();
-	        supportRequestDto.setEmail(inputs.getEmail());
-	        supportRequestDto.setSubject(inputs.getSubject());
-	        supportRequestDto.setDescription(inputs.getDescription());
-	        System.out.println(supportRequestDto);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(
+	    @RequestBody @Valid CreateSupportRequestDto inputs) {
 
-	}
-	
-		 
+	CreateSupportRequestDto supportRequestDto = new CreateSupportRequestDto();
+	supportRequestDto.setEmail(inputs.getEmail());
+	supportRequestDto.setSubject(inputs.getSubject());
+	supportRequestDto
+		.setDescription(inputs.getDescription());
+	System.out.println(supportRequestDto);
+
+    }
+
 }
